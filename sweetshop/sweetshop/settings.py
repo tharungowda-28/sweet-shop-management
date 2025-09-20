d28da2fb88a670cb3f99daaf6e335b021223317c
tharungowda-28 <tharunb22@gmail.com>
2025-09-21 00:53:56 +0530

    Changing the settings.py

--- sweetshop/sweetshop/settings.py
+++ sweetshop/sweetshop/settings.py
@@ -15,15 +15,18 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+import os
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9cy(j#uqgd(+5oyp=@g+-^=tl0dwt4+68brm5o@dj3gmbq0840'
+# SECRET_KEY = 'django-insecure-9cy(j#uqgd(+5oyp=@g+-^=tl0dwt4+68brm5o@dj3gmbq0840'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
